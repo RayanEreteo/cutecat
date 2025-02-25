e2076623e76f6e8ee4b52a85681fa4f7afbba1b3
Ereteo Rayan <90957589+RayanEreteo@users.noreply.github.com>
2025-02-25 18:19:51 +0100

    feat: add background image to Home component

--- app/page.tsx
+++ app/page.tsx
@@ -3,7 +3,8 @@ import Image from "next/image";
 export default function Home() {
   return (
     <div className="">
-
+      <div className="w-[100vw] h-[100%]  absolute -z-10 brightness-50" style={{ backgroundImage: `url(/background.jpg)`}}></div>
+      sdsd
     </div>
   );
 }
