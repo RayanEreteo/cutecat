3923edff7deda2c0bca7eb27ec627c8bfe517719
Ereteo Rayan <90957589+RayanEreteo@users.noreply.github.com>
2025-03-01 21:43:57 +0100

    feat: restructure Home component layout and add footer with source link

--- app/page.tsx
+++ app/page.tsx
@@ -2,9 +2,17 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-    <div className="">
-      <div className="w-[100vw] h-[100%]  absolute -z-10 brightness-50" style={{ backgroundImage: `url(/background.jpg)`}}></div>
-      sdsd
-    </div>
+    <>
+      <main>
+        <div className="">
+          <div className="w-[100vw] h-[100%]  absolute -z-10 brightness-50" style={{ backgroundImage: `url(/background.jpg)` }}>
+            asdasda
+          </div>
+        </div>
+      </main>
+      <footer className="absolute top-[90vh] flex items-center justify-center w-[100vw] bg-red-500 h-[10vh] underline">
+        <a href="https://github.com/RayanEreteo/cutecat" target="_blank" className="underline text-white">Accéder au code source.</a>
+      </footer>
+    </>
   );
 }
