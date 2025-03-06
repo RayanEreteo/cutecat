04f8674f950bc95461aeb717dceeb1ddf7d200c7
Ereteo Rayan <90957589+RayanEreteo@users.noreply.github.com>
2025-03-06 19:20:39 +0100

    fetch logic

--- app/page.tsx
+++ app/page.tsx
@@ -1,18 +1,59 @@
-import Image from "next/image";
+"use client"
+
+import { useState } from "react";
 
 export default function Home() {
+  const [image, setImage] = useState<string>("");
+  const [loading, setLoading] = useState<boolean>(false);
+  const [imageLoaded, setImageLoaded] = useState<boolean>(false);
+
+  async function generateImage() {
+    setLoading(true);
+    const headers = new Headers({
+      "Content-Type": "application/json",
+      "x-api-key": "live_YH5gqxK5gkD7nE7yc8ZlYuQDJ66CCLGCuEuo7u0FwZDsAmrIl95elBxjsviQNR9N"
+    });
+
+    const requestOptions: RequestInit = {
+      method: 'GET',
+      headers: headers,
+      redirect: 'follow'
+    };
+
+    try {
+      setImage("");
+      setImageLoaded(false);
+
+      const response = await fetch("https://api.thecatapi.com/v1/images/search", requestOptions);
+      const data = await response.json();
+      setImage(data[0].url);
+      setLoading(false);
+    } catch (error) {
+      setLoading(false);
+      console.error(error);
+    } finally {
+      setLoading(false);
+    }
+
+  }
+
   return (
     <>
       <main>
         <div className="">
-          <div className="w-[100vw] h-[100%]  absolute -z-10 brightness-50" style={{ backgroundImage: `url(/background.jpg)` }}>
-            asdasda
+          <div id="background" className="w-[100vw] h-[100%]  absolute -z-10 brightness-50" style={{ backgroundImage: `url(/background.jpg)` }}></div>
+          <div id="main-content" className="w-[100vw] h-[100vh] flex flex-col items-center justify-center">
+            <h1 className="text-white text-4xl font-bold">Cutecat</h1>
+            <p className="text-white text-2xl mb-4">Generate pictures of cute cat simply by clicking a button.</p>
+            {image &&  <img src={image} alt="cat" className="w-[500px] h-[500px] object-cover rounded-md" onLoad={() => setImageLoaded(true)} />}
+            {!imageLoaded && <p className="text-white text-xl">Loading...</p>}
+            <button className="bg-white text-black px-4 py-2 rounded-md hover:bg-gray-200 hover:scale-105 disabled:opacity-50 disabled:cursor-not-allowed transition-all duration-300" disabled={loading} onClick={generateImage}>Generate a cat 🐱</button>
           </div>
         </div>
       </main>
       <footer className="absolute top-[90vh] flex items-center justify-center w-[100vw] bg-red-500 h-[10vh] underline">
-        <a href="https://github.com/RayanEreteo/cutecat" target="_blank" className="underline text-white">Accéder au code source.</a>
+        <a href="https://github.com/RayanEreteo/cutecat" target="_blank" className="underline text-white">Check the source code.</a>
       </footer>
     </>
-  );
+  )
 }
